dac38b51630f2ff0604b08662dbc69736fa4701d
Oscar26-zs <oscarzswork@gmail.com>
2025-08-18 11:16:50 -0600

    feat: Wrap SocialBar component in a div for responsive visibility

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,16 +1,21 @@
-import {  Outlet } from '@tanstack/react-router'
+import { Outlet } from '@tanstack/react-router'
 import Header from '../shared/components/Header'
 import Footer from '../shared/components/Footer'
 import SocialBar from '@/shared/components/SocialBar'
 
 const Layout = () => {
   return (
-   <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <SocialBar />
+      
+      <div className="hidden md:block">
+        <SocialBar />
+      </div>
+      
       <main className="flex-1">
         <Outlet /> 
       </main>
+      
       <Footer />
     </div>
   )
