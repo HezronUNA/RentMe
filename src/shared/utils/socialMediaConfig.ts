19299c39394894576097310ff09df8cb8fee0ea6
FabiolaC1 <fabicarrera1904@gmail.com>
2025-08-19 15:27:01 -0600

    Feat: Creacion footer , fix: cambio de links socialmediaconfig

--- src/shared/utils/socialMediaConfig.ts
+++ src/shared/utils/socialMediaConfig.ts
@@ -6,29 +6,29 @@ import type { SocialConfig } from "../types/socialMedia";
  */
 export const SOCIAL_CONFIG: SocialConfig = [
   {
-    platform: 'whatsapp',
-    bg: 'bg-green-500',
-    label: 'WhatsApp',
-    phone: '50683888231',
+    platform: "whatsapp",
+    bg: "bg-green-500",
+    label: "WhatsApp",
+    phone: "50683888231",
     message:
-      '¡Hola Rent Me CR! Quiero información sobre alojamientos y administración de propiedades.',
+      "¡Hola Rent Me CR! Quiero información sobre alojamientos y administración de propiedades.",
   },
   {
-    platform: 'instagram',
-    bg: 'bg-pink-500',
-    label: 'Instagram',
-    url: 'https://www.instagram.com/rentmecr/',
+    platform: "instagram",
+    bg: "bg-pink-500",
+    label: "Instagram",
+    url: "https://www.instagram.com/rent_me_cr?utm_source=ig_web_button_share_sheet&igsh=MXRqa3A2d3BreWM0bg==",
   },
   {
-    platform: 'tiktok',
-    bg: 'bg-black',
-    label: 'TikTok',
-    url: 'https://www.tiktok.com/@rent_me_cr',
+    platform: "tiktok",
+    bg: "bg-black",
+    label: "TikTok",
+    url: "https://www.tiktok.com/@rentmecr?_t=ZM-8z19gDqmMIo&_r=1",
   },
   {
-    platform: 'facebook',
-    bg: 'bg-blue-600',
-    label: 'Facebook',
-    url: 'https://www.facebook.com/rentmecr',
+    platform: "facebook",
+    bg: "bg-blue-600",
+    label: "Facebook",
+    url: "https://www.facebook.com/profile.php?id=61564852763749&locale=es_LA",
   },
-]
+];
