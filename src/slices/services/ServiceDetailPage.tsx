b99548be0ebd6d618a4571c851d3b74163550557
Oscar26-zs <oscarzswork@gmail.com>
2025-09-08 09:04:52 -0600

    feat: add ServiceDetailPage and related routing for service details

---
+++ src/slices/services/ServiceDetailPage.tsx
@@ -0,0 +1,11 @@
+import Hero from "./sections/serviceDetail/components/Hero"
+
+const ServiceDetailPage = () => {
+  return (
+    <section>
+      <Hero/>
+   </section>
+  )
+}
+
+export default ServiceDetailPage
