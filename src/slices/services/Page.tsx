26bd245e95f9713a34db031a9aa3bc16de58e83f
FabiolaC1 <fabicarrera1904@gmail.com>
2025-09-08 22:34:24 -0600

    feat the hero service

--- src/slices/services/Page.tsx
+++ src/slices/services/Page.tsx
@@ -1,12 +1,10 @@
+import { ServiceHero } from "./sections/heroService/ServiceHero";
+
+
 export default function AboutUsPage() {
   return (
     <section>
-      <h1 className="text-3xl font-bold mb-4">Nuestros Servicios</h1>
-      <p>
-        Somos un equipo especializado en la administración integral y promoción de alojamientos
-        turísticos en Costa Rica. Nuestro enfoque es convertir cada propiedad en una experiencia
-        inolvidable para el huésped, y una operación sin estrés para el propietario.
-      </p>
+   <ServiceHero/>
     </section>
   )
 }
