aba56317cb04b3091bf4f8050f333dec80261727
FabiolaC1 <fabicarrera1904@gmail.com>
2025-09-17 21:19:57 -0600

    feat: implement CleaningServiceHero component with loading and error handling, and update AboutUsPage to include it

--- src/slices/services/Page.tsx
+++ src/slices/services/Page.tsx
@@ -1,12 +1,15 @@
+import CleaningServiceHero from "./sections/cleaningWork/CleaningWork";
 import { ServiceHero } from "./sections/heroService/ServiceHero";
 import SalesService from "./sections/salesService/SalesService";
 import WaysWork from "./sections/waysWork/WaysWork";
+
 
 export default function AboutUsPage() {
   return (
     <section>
       <ServiceHero />
       <WaysWork />
+      <CleaningServiceHero />
       <SalesService/>
     </section>
   )
