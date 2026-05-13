7e37607617ef5091c54aa6d23b3801af6b8efd77
Oscar26-zs <oscarzswork@gmail.com>
2026-05-12 20:17:54 -0600

    fix: update Tailwind CSS configuration and add typography plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path' // Asegúrate de tener instalado @types/node
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
