93c6f6b766e973a9615c1cdbcbdabb1b77cac468
Oscar26-zs <oscarzswork@gmail.com>
2026-05-24 14:14:12 -0600

    sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import  sitemap  from 'vite-plugin-sitemap'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), sitemap({
+    hostname: 'https://tudominio.com',
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
